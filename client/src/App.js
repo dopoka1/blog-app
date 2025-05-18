c05dee778684e7b3b07503ad95eaf377582fdbf2
dopoka <zyzzdr@gmail.com>
2025-05-18 15:44:30 -0500

    Add working routes and layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './header';
+import Post from './post';
+import Layout from './layout';
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className='image'>
-          <img src="https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_auto,w_1500,ar_3:2/k%2FPhoto%2FSeries%2F2024-07-how-to-make-perfect-cheesecake%2Fhow-to-make-perfect-cheesecake-319" alt=""/>
-        </div>
-        <div className='texts'>
-          <h2>Sweet Strawberry Cheesecake</h2>
-          <p className="info">
-            <a href="" className="author">dopoka</a>
-            <time datetime="">2025-05-18</time>
-          </p>
-          <p className='summary'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={
+          <Post />
+        } />
+        <Route path={'/login'} element={
+          <div>Login</div>
+        } />
+      </Route>
+    </Routes>
   );
 }
 
--- client/src/post.js
+++ client/src/post.js
@@ -2,22 +2,23 @@ import Header from "./header";
 
 export default function Post() {
     return (
-        <div className="post">
-            <div className="image">
-                <img src="https://sugarspunrun.com/wp-content/uploads/2023/06/Strawberry-cheesecake-recipe-6-of-8.jpg" alt=""/>
-            </div>
-            <div className="texts">
-                <h2>This Is The Best Cheesecake Recipe Ever</h2>
-                <p className="info">
-                    <a href="" className="author">dopoka</a>
-                    <time>2025-05-15 3:14</time>
-                </p>
-                <p className='summary'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                </p>
-            </div>
+      <div className="post">
+        <div className='image'>
+          <img src="https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_auto,w_1500,ar_3:2/k%2FPhoto%2FSeries%2F2024-07-how-to-make-perfect-cheesecake%2Fhow-to-make-perfect-cheesecake-319" alt=""/>
+        </div>
+        <div className='texts'>
+          <h2>Sweet Strawberry Cheesecake</h2>
+          <p className="info">
+            <a href="" className="author">dopoka</a>
+            <time datetime="">2025-05-18</time>
+          </p>
+          <p className='summary'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
       </div>
     );
 }
