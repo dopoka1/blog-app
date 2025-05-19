d5b0f5a73f1013a545f285842f5de205ef7d6736
dopoka <zyzzdr@gmail.com>
2025-05-19 02:43:31 -0500

    Add mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require("./models/user");
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 
-app.post('/register', (req,res) => {
+mongoose.connect('mongodb+srv://dopoka:<db_password>@#@cluster0.z6ehw4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register', async (req,res) => {
     const {username,password} = req.body;
-    res.json({requestData:{username,password}});
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
 });
 
 app.listen(4000);
