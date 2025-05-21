08e1a6e3ff60fcba566142d66ea9000efffe195d
dopoka <zyzzdr@gmail.com>
2025-05-20 20:03:31 -0500

    add user authentication

--- api/index.js
+++ api/index.js
@@ -3,15 +3,18 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require("./models/user");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 
-app.use(cors());
+const secret = 'randomstring'
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://dopoka:K00li000@cluster0.ajahihp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://dopoka:<db_pass>@cluster0.ajahihp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
     try{
@@ -22,6 +25,21 @@ app.post('/register', async (req,res) => {
         res.json(userDoc);
     } catch(e) {
         res.status(400).json(e);
+    }
+});
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passCheck = bcrypt.compareSync(password, userDoc.password);
+    if (passCheck) {
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token);
+            res.json('ok');
+        });
+    } else {
+        res.status(400).json('Invalid credentials');
     }
 });
 
