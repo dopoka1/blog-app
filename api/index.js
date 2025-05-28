d8b54aceff05a227c42b66bfd8313571e6054e5f
dopoka <zyzzdr@gmail.com>
2025-05-27 21:54:27 -0500

    outline post upload data

--- api/index.js
+++ api/index.js
@@ -60,4 +60,8 @@ app.post('/logout', (req,res) => {
     res.cookie('token', '').json('ok');
 });
 
+app.post('/post', (req,res) => {
+
+});
+
 app.listen(4000);
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,33 +2,57 @@ import ReactQuill from 'react-quill-new';
 import 'react-quill-new/dist/quill.snow.css';
 import { useState } from 'react';
 
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline','strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        ['link', 'image'],
+        ['clean']
+    ],  
+};  
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+]
+
 export default function CreatePost() {
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
-    const modules = {
-        toolbar: [
-            [{ 'header': [1, 2, false] }],
-            ['bold', 'italic', 'underline','strike', 'blockquote'],
-            [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-            ['link', 'image'],
-            ['clean']
-        ],  
-    };  
-    const formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image'
-    ]
+    const [files, setFiles] = useState('');
+    function createNewPost(ev) {
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        ev.preventDefault();
+        fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data,
+        })
+    }
     
-
     return (
-        <form>
-            <input type="title" placeholder={"title"}/>
-            <input type="summary" placeholder={"summary"}/>
-            <input type="file"/>
-            <ReactQuill value={content} modules={modules} formats={formats}/>
+        <form onSubmit={createNewPost}>
+            <input type="title" 
+                placeholder={"title"} 
+                value={title} 
+                onChange={ev => setTitle(ev.target.value)}/>
+            <input type="summary" 
+                placeholder={"summary"}
+                value={summary}
+                onChange={ev => setSummary(ev.target.value)}/>
+            <input type="file" 
+                onChange={ev => setFiles(ev.target.files)}/>
+            <ReactQuill 
+                value={content} 
+                modules={modules} 
+                formats={formats}
+                onChange={newValue => setContent(newValue)}/>
             <button style={{marginTop:'5px'}}>Post</button>
         </form>
     );
